1aa3149df722961b4a10510f573589c4b4a99df2
Simone van Gompel <simonevangompel@hotmail.com>
2015-04-11 21:21:43 +0200

    Started Exercise 1

--- A1.R
+++ A1.R
@@ -15,3 +15,20 @@ qqnorm(x4)
 
 hist(x5)
 qqnorm(x5)
+
+par(mfrow=c(1,1))
+par(pin=c(2.5,5))
+x1_2=rnorm(20)
+qqnorm(x1_2)
+
+x2_2=rnorm(100)
+qqnorm(x2_2)
+
+x3_2=rnorm(100)
+qqnorm(x3_2)
+
+x4_2=rnorm(30)
+qqnorm(x4_2)
+
+x5_2=rnorm(40)
+qqnorm(x5_2)
