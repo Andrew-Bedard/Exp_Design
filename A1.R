4459869072dcc0cd784ef274caef8addf30538b0
Simone van Gompel <simonevangompel@hotmail.com>
2015-04-12 14:29:01 +0200

    Exercise 2, only the results

--- A1.R
+++ A1.R
@@ -1,3 +1,5 @@
+# Exercise 1
+
 load(file="assign1.RData")
 
 par(mfrow=c(1,2))
@@ -18,17 +20,53 @@ qqnorm(x5)
 
 par(mfrow=c(1,1))
 par(pin=c(2.5,5))
-x1_2=rnorm(20)
-qqnorm(x1_2)
+qqnorm(rnorm(20))
 
-x2_2=rnorm(100)
-qqnorm(x2_2)
+qqnorm(rnorm(100))
 
-x3_2=rnorm(100)
-qqnorm(x3_2)
+qqnorm(rnorm(100))
 
-x4_2=rnorm(30)
-qqnorm(x4_2)
+qqnorm(rnorm(30))
 
-x5_2=rnorm(40)
-qqnorm(x5_2)
+qqnorm(rnorm(40))
+
+
+
+# Exercise 2
+#2.1
+m=30
+n=30
+mu=180
+nu=180
+sd=10
+B=1000
+p=numeric(B)
+
+for (b in 1:B) {
+  x=rnorm(m,mu,sd)
+  y=rnorm(n,nu,sd)
+  p[b]=t.test(x,y,var.equal=TRUE)[[3]]
+}
+
+sum(p<0.05)
+sum(p<0.1)
+hist(p)
+
+#2.2
+m=30
+n=30
+mu=180
+nu=180
+sd=1
+B=1000
+p=numeric(B)
+
+for (b in 1:B) {
+  x=rnorm(m,mu,sd)
+  y=rnorm(n,nu,sd)
+  p[b]=t.test(x,y,var.equal=TRUE)[[3]]
+}
+
+sum(p<0.05)
+sum(p<0.1)
+hist(p)
