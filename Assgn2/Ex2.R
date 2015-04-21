96abcf91301887efcb6a32a9f8cb8a9de4ec2629
Simone van Gompel <simonevangompel@hotmail.com>
2015-04-21 23:14:25 +0200

    finished reporting E2 and E3

--- Assgn2/Ex2.R
+++ Assgn2/Ex2.R
@@ -1,26 +1,31 @@
 data79 = read.table("light1879.txt", header=F)
 data82 = read.table("light1882.txt", header=F)
 
+#2.1
 data79 = as.matrix(data79)
 hist(data79)
 data82 = as.matrix(data82)
 hist(data82)
 
+#2.2
 error = qnorm(0.975)*sd(data79)/sqrt(length(data79))
 left = mean(data79)-error
 right = mean(data79)+error
 c(left, right)
 
+#2.3
 error = qnorm(0.975)*sd(data82)/sqrt(length(data82))
 left = mean(data82)-error
 right = mean(data82)+error
 c(left, right)
 
+#2.4
 error = qnorm(0.975)*sd(data79)/sqrt(length(data79))
 left = median(data79)-error
 right = median(data79)+error
 c(left, right)
 
+#2.5
 error = qnorm(0.975)*sd(data82)/sqrt(length(data82))
 left = median(data82)-error
 right = median(data82)+error
