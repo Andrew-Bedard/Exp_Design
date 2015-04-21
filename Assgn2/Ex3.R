d8f5a2dd3e1dd011f0ed6523051e7d5da39939c2
Simone van Gompel <simonevangompel@hotmail.com>
2015-04-21 21:17:10 +0200

    Finished 3.2

--- Assgn2/Ex3.R
+++ Assgn2/Ex3.R
@@ -1,7 +1,15 @@
 library(BSDA)
 
+#3.1
 klm = scan(file = "klm.txt")
 
+par(mfrow=c(1,2))
 hist(klm)
+boxplot(klm)
 
 SIGN.test(klm, md=35, conf.level = 0.995)
+
+#3.2
+sum(klm)
+sum(klm > 70)
+binom.test(14,3464,conf.level = 0.995)
