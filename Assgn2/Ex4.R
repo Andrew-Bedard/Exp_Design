6d6f76367d9dc2ce91801ccdd80dfc797256de92
Simone van Gompel <simonevangompel@hotmail.com>
2015-04-22 12:37:21 +0200

    Started Ex 4

---
+++ Assgn2/Ex4.R
@@ -0,0 +1,23 @@
+library(BSDA)
+
+run = read.table(file = "run.txt")
+
+before = as.numeric(run[,1])
+after = as.numeric(run[,2])
+par(mfrow=c(2,2))
+hist(before)
+boxplot(before)
+hist(after)
+boxplot(after)
+
+lemo = run[which(run[,3]=="lemo"), 1:2]
+energy = run[which(run[,3]=="energy"), 1:2]
+lemobefore = as.numeric(lemo[,1])
+energybefore = as.numeric(energy[,1])
+lemoafter = as.numeric(lemo[,2])
+energyafter = as.numeric(energy[,2])
+par(mfrow=c(2,2))
+hist(lemobefore)
+hist(lemoafter)
+hist(energybefore)
+hist(energyafter)
