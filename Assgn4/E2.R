d5d378b8de1d32c669cef2574cd1d85ed6b8f0a7
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-09 15:27:41 +0200

    progress on exercise 2

--- Assgn4/E2.R
+++ Assgn4/E2.R
@@ -7,8 +7,8 @@ time =search$time
 skill = search$skill
 interface = search$interface
 
-boxplot(time~skill)
-boxplot(time~interface)
+boxplot(time~skill,xlab="skill",ylab="time", main="Time per Skill")
+boxplot(time~interface,xlab="interface",ylab="time", main="Time per Interface")
 
 interaction.plot(skill,interface,time,lwd=2,col=rainbow(3))
 interaction.plot(interface,skill,time,lwd=2,col=rainbow(8))
@@ -18,3 +18,16 @@ kruskal.test(time,interface)
 
 #2.4
 
+xtabs(time~skill+interface,data=search)
+#2.5
+
+aovpen=lm(time~skill+interface,data=search)
+anova(aovpen)
+summary(aovpen)
+
+qqnorm(residuals(aovpen))
+plot(fitted(aovpen),residuals(aovpen))
+
+#2.6
+
+friedman.test(time,skill,interface)
