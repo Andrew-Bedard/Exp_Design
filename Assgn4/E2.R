8b2cb721d24219c43195a69e5f7ff22d27d2765e
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-09 20:38:40 +0200

    more

--- Assgn4/E2.R
+++ Assgn4/E2.R
@@ -15,15 +15,16 @@ for (i in 1:B) print(sample(1:(N*I)))
 #2.2
 
 
-boxplot(time~skill,xlab="skill",ylab="time", main="Time per Skill")
-boxplot(time~interface,xlab="interface",ylab="time", main="Time per Interface")
+boxplot(time~skill,xlab="skill",ylab="time", main="Skill vs Time")
+boxplot(time~interface,xlab="interface",ylab="time", main="Interface vs Time")
 
 interaction.plot(skill,interface,time,lwd=2,col=rainbow(3))
 interaction.plot(interface,skill,time,lwd=2,col=rainbow(8))
 
 #2.3
+interface = factor(search$interface)
+skill = factor(search$skill)
 kruskal.test(time,interface)
-#look at previous assignment for this one
 
 
 #2.4
@@ -32,13 +33,13 @@ kruskal.test(time,interface)
 #summary(srcaov)
 #srcmult = glht(srcaov,linfct=mcp(skill="Tukey"))
 
-aovsrch=lm(time~skill+interface,data=search)
+aovsrch=lm(time~skill+interface)
 anova(aovsrch)
 summary(aovsrch)
 
-anovskill = lm(time~skill,data=search)
+anovskill = lm(time~skill)
 anova(anovskill)
-anovinter = lm(time~interface, data=search)
+anovinter = lm(time~interface)
 anova(anovinter)
 #2.5
 
@@ -49,7 +50,7 @@ plot(fitted(aovsrch),residuals(aovsrch))
 
 #2.6
 
-friedman.test(time,skill,interface)
+friedman.test(time,interface,skill)
 
 #2.7
 
--- Assgn4/E3.R
+++ Assgn4/E3.R
@@ -1,5 +1,27 @@
+#Note: Interest was in the effect of the type of Yogurt
+#used as a starter!
 
-cream = read.table("cream.txt")
+
+cream = read.table("cream.txt",header=T)
+cream$starter = factor(cream$starter)
+cream$position = factor(cream$position)
 
 #3.1
 
+anovacid = lm(acidity~starter+batch+position,data=cream)
+anova(anovacid)
+
+#3.2
+summary(anovacid)
+
+#3.3
+
+#cream = read.table("cream.txt",header=T)
+library(lme4)
+creamlmer = lmer(acidity~starter+position+(1|batch),data=cream,REML=FALSE)
+
+#creamlm = lm(acidity~starter*position+batch+batch:starter,data=cream)
+summary(creamlmer)
+
+creamlmer1 = lmer(acidity~position+(1|batch),data=cream,REML=FALSE)
+anova(creamlmer1,creamlmer)
