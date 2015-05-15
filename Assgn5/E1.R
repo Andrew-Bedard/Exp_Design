20f4a74bc353718ba3455322252f72689e3971bc
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-15 12:05:10 +0200

    Figured out part 1.3

--- Assgn5/E1.R
+++ Assgn5/E1.R
@@ -10,11 +10,25 @@ for (j in 1:length(nauseatable[,1])){
     medicin = c(medicin, rep(rnames[j], nauseatable[j,i]))
   }
 }
+medicin <- factor(medicin)
 nausea.frame=data.frame(nausea,medicin)
 
 #1.2
 xtabs(~medicin+nausea)
 
 #1.3
-#??
-chisq.test(xtabs(~medicin+nausea))[[1]]
+
+teststat.obs = chisq.test(xtabs(~medicin+nausea))[[1]]
+
+B = 1000
+tstar=numeric(B)
+for (i in 1:B)
+{
+  medstar = sample(medicin) ## permuting labels
+  tstar[i] = chisq.test(xtabs(~nausea+medstar))[[1]]
+}
+
+hist(tstar)
+
+pl = sum(tstar<teststat.obs)/B
+pr = sum(tstar>teststat.obs)/B
