9d3133680abafdffaf20122520b94332b6b14032
Simone van Gompel <simonevangompel@hotmail.com>
2015-05-15 16:11:58 +0200

    Fixed merge conflict

--- Assgn5/E4.R
+++ Assgn5/E4.R
@@ -1,11 +1,10 @@
 library('corrplot')
 data = read.table("expensescrime.txt", header=T)
-
 attach(data)
+
 pairs(data)
 dataframe = data.frame(bad,crime,lawyers,employ,pop,expend)
 cor(dataframe)
-corrplot(cor(dataframe), method="shade", type="lower")
 
 summary(lm(expend~pop, data=data))
 summary(lm(expend~employ, data=data))
@@ -21,3 +20,20 @@ summary(lm(expend~employ+bad, data=data))
 summary(lm(expend~employ+lawyers+pop, data=data))
 summary(lm(expend~employ+lawyers+crime, data=data))
 summary(lm(expend~employ+lawyers+bad, data=data))
+
+datalm = lm(expend~employ+lawyers, data=data)
+round(cooks.distance(datalm),2)
+plot(cooks.distance(datalm))
+
+round(cor(data[,3:7]),2)
+
+
+plot(residuals(lm(expend~pop, data=data)),pop)
+plot(residuals(lm(expend~employ, data=data)),employ)
+plot(residuals(lm(expend~lawyers, data=data)),lawyers)
+plot(residuals(lm(expend~crime, data=data)),crime)
+plot(residuals(lm(expend~bad, data=data)),bad)
+
+# after this one the axis from the other plots will disappear
+# so this is done last
+corrplot(cor(dataframe), method="shade", type="lower")
