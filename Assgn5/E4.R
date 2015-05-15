bb4b0185ea12a50e74e8f435a8b36d85d8f5a44c
Simone van Gompel <simonevangompel@hotmail.com>
2015-05-15 17:23:49 +0200

    Finished E4

--- Assgn5/E4.R
+++ Assgn5/E4.R
@@ -2,38 +2,60 @@ library('corrplot')
 data = read.table("expensescrime.txt", header=T)
 attach(data)
 
+# looking at correlations, 
+# the correlation plot is on the last line of the program
 pairs(data)
 dataframe = data.frame(bad,crime,lawyers,employ,pop,expend)
-cor(dataframe)
+round(cor(dataframe), 2)
 
-summary(lm(expend~pop, data=data))
-summary(lm(expend~employ, data=data))
-summary(lm(expend~lawyers, data=data))
-summary(lm(expend~crime, data=data))
-summary(lm(expend~bad, data=data))
+# step-up algorithm
+# stop after second step
+summary(lm(expend~pop))
+summary(lm(expend~employ))
+summary(lm(expend~lawyers))
+summary(lm(expend~crime))
+summary(lm(expend~bad))
 
-summary(lm(expend~employ+pop, data=data))
-summary(lm(expend~employ+lawyers, data=data))
-summary(lm(expend~employ+crime, data=data))
-summary(lm(expend~employ+bad, data=data))
+summary(lm(expend~employ+pop))
+summary(lm(expend~employ+lawyers))
+summary(lm(expend~employ+crime))
+summary(lm(expend~employ+bad))
 
-summary(lm(expend~employ+lawyers+pop, data=data))
-summary(lm(expend~employ+lawyers+crime, data=data))
-summary(lm(expend~employ+lawyers+bad, data=data))
+summary(lm(expend~employ+lawyers+pop))
+summary(lm(expend~employ+lawyers+crime))
+summary(lm(expend~employ+lawyers+bad))
 
-datalm = lm(expend~employ+lawyers, data=data)
+# step-down algorithm
+# same result as step-up so continue with this
+summary(lm(expend~employ+lawyers+bad+pop+crime))
+summary(lm(expend~employ+lawyers+bad+pop))
+summary(lm(expend~employ+lawyers+bad))
+summary(lm(expend~employ+lawyers))
+
+# influence points are the ones that stand out in cooks distance
+datalm = lm(expend~employ+lawyers)
 round(cooks.distance(datalm),2)
+par(mfrow=c(1,1))
 plot(cooks.distance(datalm))
 
+# problems with colinearty, the corrplot is on the bottom
 round(cor(data[,3:7]),2)
 
+# looking at the different residuals
+par(mfrow=c(1,2))
+plot(residuals(datalm),employ)
+plot(residuals(datalm),lawyers)
+par(mfrow=c(2,2))
+plot(residuals(datalm),pop)
+plot(residuals(datalm),crime)
+plot(residuals(datalm),bad)
+par(mfrow=c(1,2))
+plot(residuals(datalm),expend)
+plot(residuals(datalm),fitted(datalm))
+par(mfrow=c(1,1))
+qqnorm(residuals(datalm))
 
-plot(residuals(lm(expend~pop, data=data)),pop)
-plot(residuals(lm(expend~employ, data=data)),employ)
-plot(residuals(lm(expend~lawyers, data=data)),lawyers)
-plot(residuals(lm(expend~crime, data=data)),crime)
-plot(residuals(lm(expend~bad, data=data)),bad)
-
-# after this one the axis from the other plots will disappear
+# after this one the axis from the other plots somehow disappeared
 # so this is done last
 corrplot(cor(dataframe), method="shade", type="lower")
+corrplot(cor(data[,3:7]), method="shade", type="lower")
