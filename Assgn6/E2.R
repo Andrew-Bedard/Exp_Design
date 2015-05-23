cb3d730d67613613eee5e83b609d45d657190a03
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-23 13:00:35 +0200

    progress on exercise 2

--- Assgn6/E2.R
+++ Assgn6/E2.R
@@ -1,8 +1,20 @@
+#2.1
 data = read.table("psi.txt", header=T)
 attach(data)
 
-pairs(data)
+#Table and histogram
+tot=xtabs(~psi+passed); tot
+hist(data[,1],main="passed")
 
-gpalm = lm(gpa~psi)
-plot(residuals(gpalm), psi)
-qqnorm(residuals(gpalm))
+#2.2
+
+#Estimation
+passedlm = glm(passed~psi+gpa,family=binomial)
+summary(passedlm)
+
+data$psi=factor(data$psi)
+data$gpa=factor(data$gpa)
+passedlm2 = glm(passed~psi+gpa, family=binomial)
+summary(passedlm2)
+
+plot(c(0,coef(passedlm2)[2:3]),type="l")
