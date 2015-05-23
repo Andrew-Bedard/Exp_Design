36cf57599b7befed0949d0ec477281046b65d0fd
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-23 13:20:52 +0200

    more progress exercise 2

--- Assgn6/E2.R
+++ Assgn6/E2.R
@@ -18,3 +18,14 @@ passedlm2 = glm(passed~psi+gpa, family=binomial)
 summary(passedlm2)
 
 plot(c(0,coef(passedlm2)[2:3]),type="l")
+
+#2.3
+#Addording to p-values, psi works
+
+#2.4
+
+#function to predict passing, x=psi, y=gpa
+passest <- function(x,y) {-11.602 + 2.338*x + 3.063*y}
+
+passest(1,3)
+passest(0,3)
