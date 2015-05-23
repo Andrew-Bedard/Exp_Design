2c86d5d30e27278a6839f566ea6d0d020b4f3685
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-23 16:12:15 +0200

    fixed logistic regression

--- Assgn6/E2.R
+++ Assgn6/E2.R
@@ -25,7 +25,23 @@ plot(c(0,coef(passedlm2)[2:3]),type="l")
 #2.4
 
 #function to predict passing, x=psi, y=gpa
-passest <- function(x,y) {-11.602 + 2.338*x + 3.063*y}
+passest <- function(x,y) {exp(-11.602 + 2.338*x + 3.063*y)}
 
+#student with gpa 3 and received psi
 passest(1,3)
+#student with gpa 3 and did not receive psi
 passest(0,3)
+
+#2.5
+
+#2.6
+x=matrix(c(3,15,8,6),2,2);x
+#if we look at tot, we can see 15 represents students that
+#did not receive psi, and did not pass
+#6 are those students who did receive psi, but still did not pass
+fisher.test(x)
+
+#test concludes that the effects of psi and pass are not
+#independant
+
+#2.7
