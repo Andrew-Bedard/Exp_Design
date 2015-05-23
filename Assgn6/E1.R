9e83e6c14fdad8489ad3ff976adc30704af73d60
Simone van Gompel <simonevangompel@hotmail.com>
2015-05-23 21:12:49 +0200

    Finished report on E1

--- Assgn6/E1.R
+++ Assgn6/E1.R
@@ -19,6 +19,12 @@ anova(activitylm)
 
 #1.4
 boxplot(loglongevity~activity)
+iso = data[activity=="isolated", ]
+low = data[activity=="low", ]
+high = data[activity=="high", ]
+summary(iso)
+summary(low)
+summary(high)
 
 #1.5
 activityThoraxlm = lm(loglongevity~activity+thorax,data=data)
@@ -26,21 +32,23 @@ activityThoraxlm = lm(loglongevity~activity+thorax,data=data)
 anova(activityThoraxlm)
 
 #1.6
-
+summary(activityThoraxlm)
 
 #1.7
+plot(loglongevity, thorax)
 par(mfrow = c(1,3))
-low = data[activity=="low", ]
-med = data[activity=="isolated", ]
-high = data[activity=="high", ]
-boxplot(loglongevity ~ thorax, data=med, ylim = c(2.8, 4.6), main="iso")
+boxplot(loglongevity ~ thorax, data=iso, ylim = c(2.8, 4.6), main="iso")
 boxplot(loglongevity ~ thorax, data=low, ylim = c(2.8, 4.6), main="low")
 boxplot(loglongevity ~ thorax, data=high, ylim = c(2.8, 4.6), main="high")
 
-#1.8
+#1.9
 par(mfrow = c(1,2))
 qqnorm(residuals(activityThoraxlm))
-plot(residuals(activityThoraxlm), fitted(activityThoraxlm))
+plot(fitted(activityThoraxlm), residuals(activityThoraxlm))
 
-#1.9
-anova(lm(longevity~activity+thorax))
+#1.10
+longevitylm = lm(longevity~activity+thorax)
+anova(longevitylm)
+par(mfrow = c(1,2))
+qqnorm(residuals(longevitylm))
+plot(fitted(longevitylm), residuals(longevitylm))
