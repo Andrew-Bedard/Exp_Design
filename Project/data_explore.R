8c1b6173caf8b546e3c30f0b653519d78b690433
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-06-01 21:44:37 +0200

    further progress on data exploration.
    scaling of data, begginings of creating linear model, although this may be premature
    judging by qq plots most of the data is not normally distributed with many outliers, more work on this needed

--- Project/data_explore.R
+++ Project/data_explore.R
@@ -23,6 +23,10 @@ data[!is.na(data$SEX) & !(data$SEX == 1|data$SEX ==2),]$SEX<-NA
 
 data[!is.na(data$LAB) & !(data$LAB < 6),]$LAB<-NA
 
+data[!is.na(data$PHOSMMOL) & !(data$PHOSMMOL < 2),]$PHOSMMOL<-NA
+
+
+
 #replacing the messed up values of lab 3 with the values divided by 10
 #seems reasonable
 data$CAMMOL[!is.na(data$CAMMOL) & (data$CAMMOL > 10)] <- (data$CAMMOL[!is.na(data$CAMMOL) & (data$CAMMOL > 10)])/10
@@ -33,3 +37,68 @@ pairs(data)
 
 #still some outliers that need to be dealt with
 #but its looking much better
+
+data$SEX <- as.factor(data$SEX)
+data$LAB <- as.factor(data$LAB)
+data$AGEGRP <- as.factor(data$AGEGRP)
+
+qqnorm(data$PHOSMMOL)
+plot(data$PHOSMMOL)
+hist(data$PHOSMMOL)
+
+data$PHOSMMOL = (data$PHOSMMOL)^2
+
+
+qqnorm(data$PHOSMMOL)
+plot(data$PHOSMMOL)
+hist(data$PHOSMMOL)
+
+qqnorm(data$CAMMOL)
+plot(data$CAMMOL)
+hist(data$CAMMOL)
+
+data$CAMMOL = (data$CAMMOL)^2
+
+qqnorm(data$CAMMOL)
+plot(data$CAMMOL)
+hist(data$CAMMOL)
+
+
+attach(data)
+
+boxplot(ALKSPHOS~AGEGRP)
+boxplot(CAMMOL~AGEGRP)
+boxplot(PHOSMMOL~AGEGRP)
+
+boxplot(ALKSPHOS~SEX)
+boxplot(CAMMOL~SEX)
+boxplot(PHOSMMOL~SEX)
+
+boxplot(ALKSPHOS~LAB)
+boxplot(CAMMOL~LAB)
+boxplot(PHOSMMOL~LAB)
+
+alklm = lm(CAMMOL~AGE+SEX+LAB+ALKSPHOS+AGEGRP+PHOSMMOL,data=data)
+summary(alklm)
+
+summary(lm(CAMMOL~AGE))$r.squared
+summary(lm(CAMMOL~SEX))$r.squared
+summary(lm(CAMMOL~ALKSPHOS))$r.squared
+summary(lm(CAMMOL~LAB))$r.squared
+summary(lm(CAMMOL~PHOSMMOL))$r.squared
+summary(lm(CAMMOL~AGEGRP))$r.squared
+
+summary(lm(CAMMOL~SEX+AGE))$r.squared
+summary(lm(CAMMOL~SEX+ALKSPHOS))$r.squared
+summary(lm(CAMMOL~SEX+LAB))$r.squared
+summary(lm(CAMMOL~SEX+PHOSMMOL))$r.squared
+summary(lm(CAMMOL~SEX+AGEGRP))$r.squared
+
+summary(lm(CAMMOL~SEX+LAB+AGE))$r.squared
+summary(lm(CAMMOL~SEX+LAB+ALKSPHOS))$r.squared
+summary(lm(CAMMOL~SEX+LAB+PHOSMMOL))$r.squared
+summary(lm(CAMMOL~SEX+LAB+AGEGRP))$r.squared
+
+summary(lm(CAMMOL~SEX+LAB+ALKSPHOS+AGE))$r.squared
+summary(lm(CAMMOL~SEX+LAB+ALKSPHOS+PHOSMMOL))$r.squared
+summary(lm(CAMMOL~SEX+LAB+ALKSPHOS+AGEGRP))$r.squared
