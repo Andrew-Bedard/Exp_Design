d45039d378d44dfc7297d6048a5f2ecd947c35a0
Simone van Gompel <simonevangompel@hotmail.com>
2015-06-03 12:25:45 +0200

    Introduction

--- Project/data_explore.R
+++ Project/data_explore.R
@@ -2,21 +2,23 @@ data = read.table('calcium.dat.txt', na.strings="_", header=TRUE)
 
 pairs(data)
 
-#from pairs you can already see, lab 3 messed up the data
-#they were probably using the wrong units
+# from pairs you can already see, lab 3 messed up the data
+# they were probably using the wrong units
 
-#we can also see there are a few lab numbers that are too
-#large to make sense
+# we can also see there are a few lab numbers that are too
+# large to make sense
 
-#few errors in sex labels
+# few errors in sex labels
 
-#few errors in age
+# few errors in age
 
 
-#Getting rid of nonsense values, like age greater than
-#110, dont ask me how this works!!! I DONT KNOWWW!!!
-data <- data.frame(data)
+# Getting rid of nonsense values, like age greater than
+# 110, dont ask me how this works!!! I DONT KNOWWW!!!
+# you don't need to cast to a data frame
+#data <- data.frame(data)
 
+# set all age data which is not na and not smaller than 110 (so the outliers) at NA
 data[!is.na(data$AGE) & !(data$AGE <= 110),]$AGE<-NA
 
 data[!is.na(data$SEX) & !(data$SEX == 1|data$SEX ==2),]$SEX<-NA
@@ -27,16 +29,16 @@ data[!is.na(data$PHOSMMOL) & !(data$PHOSMMOL < 2),]$PHOSMMOL<-NA
 
 
 
-#replacing the messed up values of lab 3 with the values divided by 10
-#seems reasonable
+# replacing the messed up values of lab 3 with the values divided by 10
+# seems reasonable
 data$CAMMOL[!is.na(data$CAMMOL) & (data$CAMMOL > 10)] <- (data$CAMMOL[!is.na(data$CAMMOL) & (data$CAMMOL > 10)])/10
 
-#new pairs with some crazy values removed
+# new pairs with some crazy values removed
 
 pairs(data)
 
-#still some outliers that need to be dealt with
-#but its looking much better
+# still some outliers that need to be dealt with
+# but its looking much better
 
 data$SEX <- as.factor(data$SEX)
 data$LAB <- as.factor(data$LAB)
